073ab151e8ac109491797142f9475d441cdd3799
cvalingam <cvalingam-r@hotmail.com>
2024-08-30 07:03:50 +0530

    feat: POTD 30-08-2024 updated.

--- solutions/N-Queen Problem/N-Queen Problem.java
+++ solutions/N-Queen Problem/N-Queen Problem.java
@@ -1,71 +1,65 @@
 import java.util.*;
 
 class Solution {
-    public List<List<String>> solveNQueens(int n) {
-        List<List<String>> ans = new ArrayList<>();
-        char[][] board = new char[n][];
+    ArrayList<ArrayList<Integer>> ans;
 
-        for (int i = 0; i < n; i++) {
-            char[] s = new char[n];
-            java.util.Arrays.fill(s, '.');
-            board[i] = s;
-        }
-
-        solve(0, board, ans, n);
-
+    public ArrayList<ArrayList<Integer>> nQueen(int n) {
+        ans = new ArrayList<>();
+        int[][] board = new int[n][n];
+        solve(0, board, n);
+        Collections.sort(ans, (a, b) -> {
+            for (int i = 0; i < a.size(); i++) {
+                if (!a.get(i).equals(b.get(i))) {
+                    return a.get(i) - b.get(i);
+                }
+            }
+            return 0;
+        });
         return ans;
     }
 
-    private void solve(int col, char[][] board, List<List<String>> ans, int n) {
-        if (col == n) {
-            ans.add(construct(board));
+    public void solve(int row, int[][] board, int n) {
+        if (row == n) {
+            makeBoard(board, n);
             return;
         }
 
-        for (int row = 0; row < n; row++) {
-            if (isSafe(row, col, board, n)) {
-                board[row][col] = 'Q';
-                solve(col + 1, board, ans, n);
-                board[row][col] = '.';
+        for (int col = 0; col < n; col++) {
+            if (isSafe(row, col, board)) {
+                board[row][col] = 1;
+                solve(row + 1, board, n);
+                board[row][col] = 0;
             }
         }
     }
 
-    private List<String> construct(char[][] board) {
-        List<String> res = new ArrayList<>();
-        for (int i = 0; i < board.length; i++)
-            res.add(new String(board[i]));
-        return res;
+    public void makeBoard(int[][] board, int n) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)
+                if (board[j][i] == 1)
+                    arr.add(j + 1);
+        }
+        ans.add(arr);
     }
 
-    private boolean isSafe(int row, int col, char[][] board, int n) {
-        int r = row, c = col;
-        // upper diagonal
-        while (r >= 0 && c >= 0) {
-            if (board[r][c] == 'Q')
+    public boolean isSafe(int row, int col, int[][] board) {
+        // checking row upwards
+        for (int i = 0; i < row; i++)
+            if (board[i][col] == 1)
                 return false;
-            r--;
-            c--;
-        }
 
-        r = row;
-        c = col;
-        // left
-        while (c >= 0) {
-            if (board[r][c] == 'Q')
+        // checking left diagonal upwards
+        int maxLeft = Math.min(row, col);
+        for (int i = 0; i <= maxLeft; i++)
+            if (board[row - i][col - i] == 1)
                 return false;
-            c--;
-        }
 
-        r = row;
-        c = col;
-        // lower diagonal
-        while (r < n && c >= 0) {
-            if (board[r][c] == 'Q')
+        // checking right diagonal upwards
+        int maxRight = Math.min(row, board.length - 1 - col);
+        for (int i = 0; i <= maxRight; i++)
+            if (board[row - i][col + i] == 1)
                 return false;
-            r++;
-            c--;
-        }
 
         return true;
     }
