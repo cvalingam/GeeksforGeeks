265b4884d60aa9023932f7e7b3a6abcba1f55952
cvalingam <cvalingam-r@hotmail.com>
2024-12-07 22:14:46 +0530

    feat: POTD 06-12-2024 added.

---
+++ solutions/Find H-Index/Find H-Index.java
@@ -0,0 +1,26 @@
+
+// User function Template for Java
+class Solution {
+    // Function to find hIndex
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int maxCitation = 0;
+        for (int citation : citations)
+            maxCitation = Math.max(maxCitation, citation);
+
+        int[] count = new int[maxCitation + 2];
+        for (int citation : citations) {
+            count[0]++;
+            count[citation + 1]--;
+        }
+        for (int i = 1; i < count.length; i++)
+            count[i] += count[i - 1];
+
+        for (int i = count.length - 1; i >= 0; i--) {
+            if (count[i] >= i)
+                return i;
+        }
+
+        return 0;
+    }
+}
