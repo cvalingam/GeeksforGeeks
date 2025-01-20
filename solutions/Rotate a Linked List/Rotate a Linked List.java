7e900583e0e182cefae381163a8d793b4edf9162
cvalingam <cvalingam-r@hotmail.com>
2025-01-20 08:56:33 +0530

    feat: POTD 19-01-2025 & 20-01-2025 added.

--- solutions/Rotate a Linked List/Rotate a Linked List.java
+++ solutions/Rotate a Linked List/Rotate a Linked List.java
@@ -33,3 +33,34 @@ class Solution {
         return newHead;
     }
 }
+
+// Another solution
+
+class AnotherSolution {
+    public Node rotate(Node head, int k) {
+        if (k == 0 || head == null)
+            return head;
+
+        Node curr = head;
+        int len = 1;
+
+        while (curr.next != null) {
+            curr = curr.next;
+            len += 1;
+        }
+
+        k %= len;
+
+        if (k == 0)
+            return head;
+        curr.next = head;
+        curr = head;
+
+        for (int i = 1; i < k; i++)
+            curr = curr.next;
+        head = curr.next;
+        curr.next = null;
+
+        return head;
+    }
+}
