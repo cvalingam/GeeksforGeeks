c3ca31b7e9efa0e877fac015266f8d14386bd4f7
cvalingam <cvalingam-r@hotmail.com>
2025-01-30 10:34:05 +0530

    feat: POTD 30-01-2025 added.

--- solutions/N-Queen Problem/N-Queen Problem.java
+++ solutions/N-Queen Problem/N-Queen Problem.java
@@ -64,3 +64,36 @@ class Solution {
         return true;
     }
 }
+
+// Variation 2
+class Solution {
+    public ArrayList<ArrayList<Integer>> nQueen(int n) {
+        ArrayList<ArrayList<Integer>> solutions = new ArrayList<>();
+        backtrack(n, 0, new ArrayList<>(), solutions);
+        return solutions;
+    }
+
+    private void backtrack(int n, int col, ArrayList<Integer> queenPositions, ArrayList<ArrayList<Integer>> solutions) {
+        if (col == n) {
+            solutions.add(new ArrayList<>(queenPositions));
+            return;
+        }
+
+        for (int row = 1; row <= n; row++) {
+            if (isSafe(queenPositions, row, col)) {
+                queenPositions.add(row);
+                backtrack(n, col + 1, queenPositions, solutions);
+                queenPositions.remove(queenPositions.size() - 1);
+            }
+        }
+    }
+
+    private boolean isSafe(ArrayList<Integer> queenPositions, int newRow, int newCol) {
+        for (int col = 0; col < queenPositions.size(); col++) {
+            int row = queenPositions.get(col);
+            if (row == newRow || Math.abs(row - newRow) == Math.abs(col - newCol))
+                return false;
+        }
+        return true;
+    }
+}
