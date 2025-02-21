3a36f0265d59616e7f6ccf9401d58d8672e7225b
cvalingam <cvalingam-r@hotmail.com>
2025-02-21 08:52:38 +0530

    feat: POTD 21-02-2025 added.

--- solutions/Parenthesis Checker/Parenthesis Checker.java
+++ solutions/Parenthesis Checker/Parenthesis Checker.java
@@ -1,22 +1,21 @@
-import java.util.*;
-
 class Solution {
-    // Function to check if brackets are balanced or not.
-    static boolean ispar(String x) {
+    static boolean isBalanced(String s) {
         Stack<Character> st = new Stack<>();
 
-        for (int i = 0; i < x.length(); i++) {
-            char ch = x.charAt(i);
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt((i));
 
-            if (st.isEmpty() || ch == '(' || ch == '{' || ch == '[')
+            if (ch == '(' || ch == '{' || ch == '[')
                 st.push(ch);
-            else {
-                if ((st.peek() == '(' && ch == ')') || (st.peek() == '{' && ch == '}')
-                        || (st.peek() == '[' && ch == ']'))
-                    st.pop();
-                else
-                    st.push(ch);
-            }
+
+            else if (!st.isEmpty() && st.peek() == '(' && ch == ')')
+                st.pop();
+            else if (!st.isEmpty() && st.peek() == '{' && ch == '}')
+                st.pop();
+            else if (!st.isEmpty() && st.peek() == '[' && ch == ']')
+                st.pop();
+            else
+                st.push(ch);
         }
 
         return st.isEmpty();
