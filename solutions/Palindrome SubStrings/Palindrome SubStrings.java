d623f743159b3df9e49286da0b9109012b84bca6
cvalingam <cvalingam-r@hotmail.com>
2025-03-09 08:46:32 +0530

    feat: POTD 09-03-2025 added.

---
+++ solutions/Palindrome SubStrings/Palindrome SubStrings.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int countPS(String s) {
+        int n = s.length();
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            cnt += isPalindrome(i, i, s);
+            cnt += isPalindrome(i, i + 1, s);
+        }
+
+        return cnt;
+    }
+
+    public int isPalindrome(int s, int e, String temp) {
+        int cnt = 0;
+        int n = temp.length();
+        
+        while (s >= 0 && e < n && temp.charAt(s) == temp.charAt(e)) {
+            if (e - s + 1 >= 2)
+                cnt++;
+            s--;
+            e++;
+        }
+
+        return cnt;
+    }
+}
