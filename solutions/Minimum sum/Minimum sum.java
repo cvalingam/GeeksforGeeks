1c9825d9d47a5225250c43c76e369ebcbbb9ed17
cvalingam <cvalingam-r@hotmail.com>
2025-06-23 09:03:11 +0530

    feat: POTD 23-06-2025 added

--- solutions/Minimum sum/Minimum sum.java
+++ solutions/Minimum sum/Minimum sum.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Solution {
     String minSum(int[] arr) {
         Arrays.sort(arr);
@@ -31,5 +33,39 @@ class Solution {
             sb.deleteCharAt(0);
         // return the string by converting StringBuilder to String
         return sb.toString();
+    }
+}
+
+class Solution1 {
+    String minSum(int[] arr) {
+        Arrays.sort(arr);
+
+        StringBuilder num1 = new StringBuilder();
+        StringBuilder num2 = new StringBuilder();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 0)
+                num1.append(arr[i]);
+            else
+                num2.append(arr[i]);
+        }
+
+        return addStrings(num1.toString(), num2.toString());
+    }
+
+    String addStrings(String a, String b) {
+        StringBuilder result = new StringBuilder();
+        int i = a.length() - 1, j = b.length() - 1, carry = 0;
+
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int digit1 = (i >= 0) ? a.charAt(i--) - '0' : 0;
+            int digit2 = (j >= 0) ? b.charAt(j--) - '0' : 0;
+
+            int sum = digit1 + digit2 + carry;
+            result.append(sum % 10);
+            carry = sum / 10;
+        }
+
+        return result.reverse().toString().replaceFirst("^0+(?!$)", "");
     }
 }
