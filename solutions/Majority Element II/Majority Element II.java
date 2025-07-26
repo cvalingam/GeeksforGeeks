bfb55beef5dd4d34ffe007125a8bd6b3ea0b8c40
cvalingam <cvalingam-r@hotmail.com>
2025-07-26 08:04:03 +0530

    feat: POTD 26-07-2025 added

--- solutions/Majority Element II/Majority Element II.java
+++ solutions/Majority Element II/Majority Element II.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Solution {
     // Function to find the majority elements in the array
     public List<Integer> findMajority(int[] nums) {
@@ -14,5 +16,24 @@ class Solution {
         }
 
         return result;
+    }
+}
+
+// Version 2
+class Solution1 {
+    public ArrayList<Integer> findMajority(int[] arr) {
+        int tot = arr.length;
+        ArrayList<Integer> ans = new ArrayList<>();
+        TreeMap<Integer, Integer> mp = new TreeMap<>();
+
+        for (int n : arr)
+            mp.put(n, mp.getOrDefault(n, 0) + 1);
+
+        for (int k : mp.keySet()) {
+            if (tot / 3 < mp.get(k))
+                ans.add(k);
+        }
+
+        return ans;
     }
 }
