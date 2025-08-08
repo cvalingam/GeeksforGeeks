6890a394f89c23684e05a891a3a7d527d47766d0
cvalingam <cvalingam-r@hotmail.com>
2025-08-08 08:42:03 +0530

    feat: POTD 08-08-2025 added

--- solutions/Longest Prefix Suffix/Longest Prefix Suffix.java
+++ solutions/Longest Prefix Suffix/Longest Prefix Suffix.java
@@ -16,3 +16,28 @@ class Solution {
         return preArry[str.length() - 1];
     }
 }
+
+class Solution1 {
+    int getLPSLength(String s) {
+        int n = s.length();
+        int[] lps = new int[n];
+        int len = 0;
+        int i = 1;
+        while (i < n) {
+            if (s.charAt(i) == s.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0)
+                    len = lps[len - 1];
+                else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        
+        return lps[n - 1];
+    }
+}
