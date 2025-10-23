091ddebd4a760a8c33581ee55aedad8f594403a1
cvalingam <cvalingam-r@hotmail.com>
2025-10-23 11:00:52 +0530

    feat: POTD 23-10-2025 added

--- solutions/K Closest Points to Origin/K Closest Points to Origin.java
+++ solutions/K Closest Points to Origin/K Closest Points to Origin.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Solution {
 
     class Pair implements Comparable<Pair> {
@@ -36,5 +38,31 @@ class Solution {
             ans[k][1] = curr.y;
         }
         return ans;
+    }
+}
+
+// Version 2
+class Solution1 {
+    public ArrayList<ArrayList<Integer>> kClosest(int[][] points, int k) {
+        Arrays.sort(points, (p1, p2) -> {
+            int dist1 = p1[0] * p1[0] + p1[1] * p1[1];
+            int dist2 = p2[0] * p2[0] + p2[1] * p2[1];
+
+            return Integer.compare(dist1, dist2);
+        });
+
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+
+        for (int i = 0; i < k; i++) {
+            int[] point = points[i];
+
+            ArrayList<Integer> pointList = new ArrayList<>();
+            pointList.add(point[0]);
+            pointList.add(point[1]);
+
+            res.add(pointList);
+        }
+
+        return res;
     }
 }
