14a01728496b44484c0079a37ee9c87186bc2880
cvalingam <cvalingam-r@hotmail.com>
2025-10-26 15:34:17 +0530

    feat: POTD 26-10-2025 added

--- solutions/Minimum Cost of ropes/Minimum Cost of ropes.java
+++ solutions/Minimum Cost of ropes/Minimum Cost of ropes.java
@@ -5,9 +5,8 @@ class Solution {
     public long minCost(long[] arr) {
         PriorityQueue<Long> pq = new PriorityQueue<>();
 
-        for (long val : arr) {
+        for (long val : arr)
             pq.add(val);
-        }
 
         long totalCost = 0;
 
@@ -16,6 +15,31 @@ class Solution {
             long second = pq.poll();
 
             long cost = first + second;
+            totalCost += cost;
+
+            pq.add(cost);
+        }
+
+        return totalCost;
+    }
+}
+
+// Version 2
+class Solution1 {
+    // Function to return the minimum cost of connecting the ropes.
+    public long minCost(int[] arr) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int val : arr)
+            pq.add(val);
+
+        long totalCost = 0;
+
+        while (pq.size() > 1) {
+            int first = pq.poll();
+            int second = pq.poll();
+
+            int cost = first + second;
             totalCost += cost;
 
             pq.add(cost);
