b92fb0a88f522e7c306ca7233903c19183adfb22
cvalingam <cvalingam-r@hotmail.com>
2025-12-31 12:24:23 +0530

    feat: POTD 31-12-2025 added

--- solutions/Palindrome Linked List/Palindrome Linked List.java
+++ solutions/Palindrome Linked List/Palindrome Linked List.java
@@ -48,3 +48,45 @@ class Solution {
         return true;
     }
 }
+
+// Version 2
+class Solution1 {
+    public boolean isPalindrome(Node head) {
+        if (head.next == null)
+            return true;
+
+        Node fast = head, slow = head, prev = null, nextStart = null;
+        while (fast != null) {
+            fast = fast.next;
+            if (fast == null) { // edge case -> odd no of items, dont consider middle
+                nextStart = slow.next;
+                slow = prev;
+                break;
+            }
+            if (fast.next == null)
+                break;
+            fast = fast.next;
+
+            // reverse previous pointers of slow
+            Node temp = slow;
+            slow = slow.next;
+            temp.next = prev;
+            prev = temp;
+        }
+
+        if (slow != null && nextStart == null) {
+            nextStart = slow.next;
+            slow.next = prev;
+        }
+
+        // simple check for palindrome
+        while (nextStart != null && slow != null) {
+            if (nextStart.data != slow.data)
+                return false;
+            nextStart = nextStart.next;
+            slow = slow.next;
+        }
+
+        return slow == null && nextStart == null;
+    }
+}
