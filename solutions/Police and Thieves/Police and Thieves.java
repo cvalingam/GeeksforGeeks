e4e82b3249a7adc389b8de62c1985a8444c899c7
cvalingam <cvalingam-r@hotmail.com>
2026-01-14 11:22:34 +0530

    feat: POTD 14-01-2026 added

--- solutions/Police and Thieves/Police and Thieves.java
+++ solutions/Police and Thieves/Police and Thieves.java
@@ -20,3 +20,28 @@ class Solution {
         return ans;
     }
 }
+
+//Version 2
+class Solution1 {
+    public int catchThieves(char[] arr, int k) {
+        int n = arr.length;
+        int i = 0, j = 0, count = 0;
+
+        while (i < n && j < n) {
+            while (i < n && arr[i] != 'P')
+                i++;
+            while (j < n && arr[j] != 'T')
+                j++;
+            if (i < n && j < n && Math.abs(i - j) <= k) {
+                count++;
+                i++;
+                j++;
+            } else if (j < n && j < i)
+                j++;
+            else if (i < n && i < j)
+                i++;
+
+        }
+        return count;
+    }
+}
