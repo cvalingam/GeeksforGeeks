12eaffa042c71a8012dfddb57bd49a55421f2161
cvalingam <cvalingam-r@hotmail.com>
2026-02-23 11:30:18 +0530

    feat: POTD 23-02-2026 added

--- solutions/Union of Arrays with Duplicates/Union of Arrays with Duplicates.java
+++ solutions/Union of Arrays with Duplicates/Union of Arrays with Duplicates.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Solution {
     public static int findUnion(int a[], int b[]) {
         Set<Integer> set = new HashSet<>();
@@ -7,5 +9,17 @@ class Solution {
             set.add(ele);
 
         return set.size();
+    }
+}
+
+class Solution1 {
+    public static ArrayList<Integer> findUnion(int[] a, int[] b) {
+        Set<Integer> set = new HashSet<>();
+        for (int ele : a)
+            set.add(ele);
+        for (int ele : b)
+            set.add(ele);
+
+        return new ArrayList<Integer>(set);
     }
 }
