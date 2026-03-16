eecbd24791e599f1f344837f5dc2425b7a46e105
cvalingam <cvalingam-r@hotmail.com>
2026-03-16 11:49:10 +0530

    feat: POTD 16-03-2026 added

--- solutions/K Sum Paths/K Sum Paths.java
+++ solutions/K Sum Paths/K Sum Paths.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Node {
     int data;
     Node left;
@@ -39,5 +41,39 @@ class Solution {
         map.put(sum, map.get(sum) - 1);
         if (map.get(sum) == 0)
             map.remove(sum);
+    }
+}
+
+// Solution 2
+class Solution1 {
+    public int helper(Node node, int k, int currentSum) {
+        if (node == null)
+            return 0;
+
+        int count = 0;
+        currentSum += node.data;
+        if (currentSum == k)
+            count++;
+
+        count += helper(node.left, k, currentSum);
+        count += helper(node.right, k, currentSum);
+
+        return count;
+    }
+
+    public int dfs(Node node, int k) {
+        if (node == null)
+            return 0;
+
+        int currentSum = helper(node, k, 0);
+
+        currentSum += dfs(node.left, k);
+        currentSum += dfs(node.right, k);
+
+        return currentSum;
+    }
+
+    public int countAllPaths(Node root, int k) {
+        return dfs(root, k);
     }
 }
