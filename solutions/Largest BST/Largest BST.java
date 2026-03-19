8341993d94a6100415fe2b060f30345b2079ed49
Sivalingam <cvalingam-r@hotmail.com>
2026-03-19 11:51:01 +0530

    feat: POTD 19-03-2026 added

--- solutions/Largest BST/Largest BST.java
+++ solutions/Largest BST/Largest BST.java
@@ -38,3 +38,52 @@ class Solution {
         return 1 + countNodes(node.left) + countNodes(node.right);
     }
 }
+
+// Version 2
+
+class Solution1 {
+    // Return the size of the largest sub-tree which is also a BST
+    static int largestBst(Node root) {
+        maxSize = 0;
+        solve(root);
+        return maxSize;
+    }
+
+    static class Info {
+        int size;
+        int min;
+        int max;
+        boolean isBST;
+
+        Info(int size, int min, int max, boolean isBST) {
+            this.size = size;
+            this.min = min;
+            this.max = max;
+            this.isBST = isBST;
+        }
+    }
+
+    static int maxSize = 0;
+
+    static Info solve(Node root) {
+        if (root == null)
+            return new Info(0, Integer.MAX_VALUE, Integer.MIN_VALUE, true);
+
+        Info left = solve(root.left);
+        Info right = solve(root.right);
+
+        // Check if current subtree is BST
+        if (left.isBST && right.isBST && root.data > left.max && root.data < right.min) {
+            int currSize = left.size + right.size + 1;
+            maxSize = Math.max(maxSize, currSize);
+            return new Info(
+                    currSize,
+                    Math.min(root.data, left.min),
+                    Math.max(root.data, right.max),
+                    true);
+        }
+
+        // Not a BST
+        return new Info(0, 0, 0, false);
+    }
+}
